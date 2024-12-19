ddaf1870525b204e2cf4f045d3b757ecf8e14669
Rustam Mamiy <mamiyrustam0@gmai.com>
2024-12-19 19:40:43 +0300

    получение данных о маршруте

--- site/js/mapForm.js
+++ site/js/mapForm.js
@@ -1,38 +1,3 @@
-document.getElementById('battery_analysis').onsubmit = async (e) => {
-    e.preventDefault(); // Предотвращаем стандартное поведение формы
-
-    // Собираем данные формы вручную как объект (не FormData)
-    const formElement = document.querySelector('#battery_analysis');
-
-    const formDataObject = Object.fromEntries(new FormData(formElement).entries());
-
-    const jsonData = JSON.stringify(formDataObject); // Преобразуем объект в JSON
-
-    console.log('Данные формы в JSON:', jsonData); // Проверяем данные перед отправкой
-
-    try {
-        let response = await fetch('/submit-data', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json' // Указываем Content-Type
-            },
-            body: jsonData // Передаём JSON-данные
-        });
-
-        if (response.ok) {
-            const result = await response.json();
-            alert(`Успех: ${result.message}`);
-        } else {
-            const error = await response.json();
-            alert(`Ошибка: ${error.error || 'Неизвестная ошибка'}`);
-        }
-    } catch (error) {
-        alert(`Произошла ошибка: ${error.message}`);
-    }
-};
-
-
-
 document.getElementById('mapForm').onsubmit = async (e) => {
     e.preventDefault(); // Предотвращаем стандартное поведение формы
 
@@ -63,6 +28,17 @@ document.getElementById('mapForm').onsubmit = async (e) => {
             const error = await response.json();
             alert(`Ошибка: ${error.error || 'Неизвестная ошибка'}`);
         }
+
+        if (response.ok) {
+            const updatedData = await response.json();
+            console.log('Обновленные данные из базы:', updatedData);
+            updateUI(updatedData); // Функция для обновления интерфейса новыми данными
+        } else {
+            const error = await response.json();
+            alert(`Ошибка: ${error.error || 'Неизвестная ошибка'}`);
+        }
+
+
     } catch (error) {
         alert(`Произошла ошибка: ${error.message}`);
     }
